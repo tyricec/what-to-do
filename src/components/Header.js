f317ff7f1b087ff99e10e756db94d4b0f500f1f7
Tyrice Clark <tyricec@civicresource.com>
2019-04-07 10:07:56 -0700

    Add hamburger icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,22 @@ import styled from "styled-components";
 const HeaderContainer = styled.header`
   background-color: #45ac4f;
   overflow: auto;
+  flexdirection: "column";
 `;
 
 const Title = styled.h1`
   color: #ffffff;
   font-size: 1.6em;
-  margin-left: 16px;
+  display: inline-block;
 `;
 
 const Header = () => (
   <HeaderContainer>
+    <button class="hamburger hamburger--slider" type="button">
+      <span class="hamburger-box">
+        <span class="hamburger-inner" />
+      </span>
+    </button>
     <Title>What Todo</Title>
   </HeaderContainer>
 );
